1ceffc10b85dd80c76f49bca499ff9dd288a8f37
MarGawel <gawel007@gmail.com>
2021-05-12 00:31:32 +0200

    Praca domowa - Zadanie 4

---
+++ src/pracaDomowa/User.java
@@ -0,0 +1,23 @@
+package pracaDomowa;
+
+import java.util.Arrays;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Book[] books;
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = new Book[0];
+    }
+
+    public void addBook(Book book){
+        this.books = Arrays.copyOf(this.books, this.books.length + 1);
+        this.books[this.books.length - 1] = book;
+    }
+
+}
