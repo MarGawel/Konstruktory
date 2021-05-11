fc6d36d82bd132d8f66418014e497ca7bc01c16c
MarGawel <gawel007@gmail.com>
2021-05-12 00:03:29 +0200

    Praca domowa - Zadanie 3

---
+++ src/pracaDomowa/Book.java
@@ -0,0 +1,70 @@
+package pracaDomowa;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private boolean available = true;
+    private Author author;
+    private Author[] additionalAuthors = new Author[0];
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Book(int id, String title, Author author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(int id, String title, Author author, Author[] additionalAuthors) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.additionalAuthors = additionalAuthors;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Author[] getAdditionalAuthors() {
+        return additionalAuthors;
+    }
+
+    public void setAdditionalAuthors(Author[] additionalAuthors) {
+        this.additionalAuthors = additionalAuthors;
+    }
+}
+
+
